8e8a5aad97ba3da03721b98f66b2addf3109c4c8
burkeg <burkeg@purdue.edu>
2015-03-07 17:38:27 -0500

    Fixed NameGenerator
    made overloaded method makeName() instead of overloaded constructor

--- src/NameGenerator.java
+++ src/NameGenerator.java
@@ -1,39 +1,44 @@
 public class NameGenerator {
 	
-	String[] conBlends = { "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr",
+	static String[] conBlends = { "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr",
 			"gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk",
 			"sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr" };
-	String[] vowBlends = { "ai", "au", "aw", "ay", "ea", "ee", "ei", "eu",
+	static String[] vowBlends = { "ai", "au", "aw", "ay", "ea", "ee", "ei", "eu",
 			"ew", "ey", "ie", "oi", "oo", "ou", "ow", "oy" };
-	int letterRangeLower;
-	int letterRangeUpper;
 	
-	public NameGenerator(int letterRangeLower, int letterRangeUpper) {
-		this.letterRangeLower = letterRangeLower;
-		this.letterRangeUpper = letterRangeUpper;
-		System.out.println("hi");
-	}
-	
-	/*public NameGenerator() {
-		this.letterRangeLower = 3;
-		this.letterRangeUpper = 5;
-	}*/
-
-	public String getVowel() {
+	public static String getVowel() {
 
 		int rand = (int) (Math.random() * vowBlends.length);
 
 		return vowBlends[rand];
 	}
 
-	public String getConsonant() {
+	public static String getConsonant() {
 
 		int rand = (int) (Math.random() * conBlends.length);
 
 		return conBlends[rand];
 	}
 
-	public String makeName() {
+	public static String makeName() {
+		String name = "";
+		boolean startVowel = false;
+		int rand = (int) (Math.random() * 2 + 3);
+		for (int i = 0; i < rand; i++) {
+			// if (Math.random() + (weightedValue* 4.0/10.0)< 0.5) {
+			if (startVowel) {
+				name += getVowel();
+				startVowel = true;
+			} else {
+				name += getConsonant();
+				startVowel = false;
+			}
+		}
+
+		return name;
+	}
+	
+	public static String makeName(int letterRangeLower, int letterRangeUpper) {
 		String name = "";
 		boolean startVowel = false;
 		int rand = (int) (Math.random() * (letterRangeUpper - letterRangeLower) + letterRangeLower);
