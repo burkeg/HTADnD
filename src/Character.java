e14c65ec20130ff659dc00c9bdfb251c1093246a
Lippseric <lippseric@gmail.com>
2015-03-07 01:36:44 -0500

    Updated Character Files

--- src/Character.java
+++ src/Character.java
@@ -1,6 +1,10 @@
 
-public class Character {
-	public static void main(String[] args) {
-		System.out.print("Hi.");
+public class DnDCharacter {
+
+	public static void main(String[] args) 
+	{
+
+
 	}
+
 }
---
+++ src/DnDCharacter.java
@@ -0,0 +1,15 @@
+public class DnDCharacter {
+	String chaName = new String();
+	String chaRace = new String();
+	String chaClass = new String();
+	int Diety = 0;
+	int Alignment = 0;
+	int level = 0;
+	
+	// TODO Auto-generated method stub
+	public DnDCharacter(String cName, String cClass, String cRace)
+	{
+		
+	}
+
+}
