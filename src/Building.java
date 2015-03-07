d0f91741f2bea3832a8876db8772f705b77f19ff
mlung <mlung@purdue.edu>
2015-03-07 03:11:05 -0500

    Building: Accessors

--- src/Building.java
+++ src/Building.java
@@ -3,18 +3,55 @@
 public class Building {
 	//MEMBERS:
 	//Name of building type
-	String buildingType;
+	private String buildingType;
 	
 	//Weight (likelihood of appearing)
-	int randomWeight;
+	private int randomWeight;
 	
 	//Size (maybe not)
 	
 	//Minimum number of this building
-	int buildingMin;
+	private int buildingMin;
 	
 	//Maximum number of this building
-	int buildingMax;
+	private int buildingMax;
+	
+	void setBuildingType(String type)
+	{
+		buildingType = type;
+	}
+	
+	String getBuildingType()
+	{
+		return builtingType;
+	}
+
+	int getRandomWeight()
+	{
+		return randomWeight;
+	}
+	
+	void setBuildingMin(int min)
+	{
+		if(min >= 0)
+		{
+			buildingMin = min;
+		}
+	}
+	
+	int getBuildingMin()
+	{
+		return buildingMin;
+	}
+	
+	void setBuildingMax(int max)
+	{
+		if(max > buildingMin && max >= 0)
+		{
+			buildingMax = max;
+		}
+	}
+	}
 	
 	//METHODS:
 	//Determine building type
