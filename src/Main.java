4eae91e3aee8512c7e7a932475e2617ab9f11876
burkeg <burkeg@purdue.edu>
2015-03-07 05:41:12 -0500

    Fixed up NameGenerator

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+	public static void main(String[] args) {
+		// NameGenerator n = new NameGenerator();
+		for (int i = 0; i < 10; i++) {
+			System.out.println(NameGenerator.makeName());
+		}
+	}
+
+}
