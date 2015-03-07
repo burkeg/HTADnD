4a1c634aa6de04d5f6d774ce38af8552b7cfb730
Lippseric <lippseric@gmail.com>
2015-03-07 02:47:37 -0500

    DND Character gets n sets
    added gets n sets

--- src/DnDCharacter.java
+++ src/DnDCharacter.java
@@ -1,15 +1,103 @@
 public class DnDCharacter {
-	String chaName = new String();
-	String chaRace = new String();
-	String chaClass = new String();
-	int Diety = 0;
-	int Alignment = 0;
-	int level = 0;
+	private String chaName = new String();
+	private String chaRace = new String();
+	private String chaClass = new String();
+	private String chaDiety = 0;
+	private String chaAlignment = 0;
+	private int chaLevel = 0;
 	
 	// TODO Auto-generated method stub
-	public DnDCharacter(String cName, String cClass, String cRace)
+	
+	public DnDCharacter(String cName)
 	{
-		
+		chaName = cName;
+	}
+	
+	public DnDCharacter(String cName, int clevel)
+	{
+		chaName = cName;
+		chaLevel = clevel;
+	}
+	
+	public DnDCharacter(String cName, String cRace, String cClass, int clevel)
+	{
+		chaName = cName;
+		chaRace = cRace;
+		chaClass = cClass;
+		chaLevel = clevel;
+	}
+	
+	public DnDCharacter(String cName, String cRace, String cClass, String cDiety,
+			String cAlign, int cLevel)
+	{
+		chaName = cName;
+		chaRace = cRace;
+		chaClass = cClass;
+		chaDiety = cDiety;
+		chaAlignment = cAlign;
+		chaLevel = cLevel;
+	}
+	
+	
+	// the get and set for Character NAME
+	public String getCName()
+	{
+		return chaName;
+	}
+	
+	public void setCName(String cName)
+	{
+		chaName = cName;
 	}
 
+	// the get and set for Character RACE
+	public String getCRace()
+	{
+		return chaRace;
+	}
+	public void setCRace(String cRace)
+	{
+		chaName = cRace; 
+	}
+	
+	// the get and set for Character CLASS
+	public String getCClass()
+	{
+		return chaClass;
+	}
+	public void setClass(String cClass)
+	{
+		chaClass = cClass; 
+	}
+	
+	// the get and set for character DIETY
+	public String getCDiety()
+	{
+		return chaDiety;
+	}
+	public void setCDiety(String cDiety)
+	{
+		chaDiety = cDiety; 
+	}
+	
+	// the get and set for character ALIGNMENT
+	public String getCAlignment()
+	{
+		return chaAlignment;
+	}
+	public void setCAlignment(String cAlign)
+	{
+		chaAlignment = cAlign;
+	}
+	
+	// the get and set for character LEVEL
+	public int getCLevel()
+	{
+		return chaLevel;
+	}
+	public void setCLevel(int cLevel)
+	{
+		chaLevel = cLevel;
+	}
+	
 }
