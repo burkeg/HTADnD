46d71f668a0239ef770a115086d965ce2311be23
mlung <mlung@purdue.edu>
2015-03-07 02:06:15 -0500

    Basic file handling

---
+++ src/FileHandling.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+public class FileHandling {
+	public List<String> readFile (String fileName) throws IOException
+	{
+		return Files.readAllLines(Paths.get(fileName), StandardCharsets.UTF_8);
+		
+	}
+	
+	public void writeFile (List<String> lines, String fileName) throws IOException
+	{
+		Files.write(Paths.get(fileName), lines, StandardCharsets.UTF_8);
+	}
+}
