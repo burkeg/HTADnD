146a61121625e3489c7743b9badf341cf1f3a875
Jackson Beard <Jackson.beard@yahoo.com>
2015-03-07 17:45:42 -0500

    Whatever

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
-public class Main {
 
-	public static void main(String[] args) {
-		// NameGenerator n = new NameGenerator();
-		for (int i = 0; i < 11; i++) {
-			System.out.println(NameGenerator.makeName());
-		}
+public class main 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
 	}
 
 }
