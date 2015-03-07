662efd2747baceda3e42bab519c740ac7cd6fa12
mlung <mlung@purdue.edu>
2015-03-07 18:21:49 -0500

    Fixed main()... again

--- src/Main.java
+++ src/Main.java
@@ -23,18 +23,10 @@ public class Main {
 		// these are "guards" and other name-less
 		bossCount = playerCount / 2 + (randNum(((playerCount / 3) * -1), (playerCount / 3))); 
 		// determines the amount of boss enemies for each encounter
-
-		int playerCount = 0; //this is the count of players
-	    int enemyCount = 0; // this is the count of enemies that should be made for the adventure. these are guards and other nameless characters. 
-	    int bossCount = 0; // this is the count of enemies that are actually named. the count of these are NOT considered in the enemycount.
 	    NameGenerator gen = new NameGenerator();
 	    RandomElements elements = new RandomElements();
-		
-		enemyCount = playerCount *3 + (randNum((playerCount*-1), playerCount)); //determines the amount of enemies for each encounter. 
-		//these are "guards" and other name-less enemies
-		bossCount = playerCount/2 + (randNum(((playerCount/3)*-1), (playerCount/3))); // determines the amount of boss enemies for each encounter
-		
-		List<String> enemyNames = new ArrayList<String>();
+
+	    List<String> enemyNames = new ArrayList<String>();
 		List<String> bossNames = new ArrayList<String>();
 		
 		for(int i = 0; i < bossCount; i++)
@@ -45,17 +37,8 @@ public class Main {
 		for(int i = 0; i < enemyCount; i++)
 		{
 			enemyNames.add(elements.listEnemyClasses.get(randNum(0, elements.listEnemyClasses.size())));
-=======
-		List<String> EnemyNames = new ArrayList<String>();
-		List<String> BossNames = new ArrayList<String>();
-
-		for (int i = 0; i < enemyCount; i++) {
-
-			EnemyNames.add(NameGenerator.makeName());
->>>>>>> origin/master
 		}
-		
-	}
+}
 
 	static int randNum(int min, int max) {
 		Random rand = new Random();
