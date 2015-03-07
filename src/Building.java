bdce4cf28c59a8b11e9d0202eeb6057a59e2ec8d
mlung <mlung@purdue.edu>
2015-03-07 02:49:08 -0500

    Empty building/village classes
    Comments describing the blueprint for the building and village classes

---
+++ src/Building.java
@@ -0,0 +1,13 @@
+//Class for buildings
+
+public class Building {
+	//MEMBERS:
+	//Name of building type
+	//Weight (likelihood of appearing)
+	//Size
+	//Minimum number of this building
+	//Maximum number of this building
+	
+	//METHODS:
+	//Determine building type
+}
---
+++ src/Village.java
@@ -0,0 +1,14 @@
+//Class for villages
+
+public class Village {
+	//MEMBERS:
+	//Village total size
+	//Village buildings (TODO: BUILDING CLASS)
+	//Any other things?
+	
+	//METHODS:
+	//Create village
+	//Select village building
+}
+
+ 
