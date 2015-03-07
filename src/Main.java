41ee385317e3de6ee5277dbe5b061ef1e3d20981
burkeg <burkeg@purdue.edu>
2015-03-07 17:04:19 -0500

    Added Constructors to NameGenerator
    NameGenerator() defaults to 3 to 5, 2 letter chunks
    NameGenerator(lowerLimit, upperLimit) //amount of 2 letter chunks

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// NameGenerator n = new NameGenerator();
+		NameGenerator locationName = new NameGenerator(15, 20);
 		for (int i = 0; i < 11; i++) {
-			System.out.println(NameGenerator.makeName());
+			System.out.println(locationName.makeName());
 		}
 	}
 
