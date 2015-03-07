0fd73bdfdee9c5631469425e5eee431e395e7b75
Jackson Beard <Jackson.beard@yahoo.com>
2015-03-07 17:45:49 -0500

    Revert "Whatever"
    This reverts commit 146a61121625e3489c7743b9badf341cf1f3a875.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
+public class Main {
 
-public class main 
-{
-
-	public static void main(String[] args) 
-	{
-		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+	public static void main(String[] args) {
+		// NameGenerator n = new NameGenerator();
+		for (int i = 0; i < 11; i++) {
+			System.out.println(NameGenerator.makeName());
+		}
 	}
 
 }
