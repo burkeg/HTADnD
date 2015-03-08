f680a3f0ba6f50136e3da638425011810791da64
burkeg <burkeg@purdue.edu>
2015-03-08 00:34:49 -0500

    mistake oops

--- src/MapDraw.java
+++ src/MapDraw.java
@@ -4,11 +4,17 @@ public class MapDraw {
 	int width;
 	int height;
 	BufferedImage bi;
+	MapGenerator genMap;
 	
-	public MapDraw(int width, int height) {
+	public MapDraw(int width, int height, MapGenerator map) {
 		this.width = width;
 		this.height = height;
 		bi =  new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+		genMap = new MapGenerator(width, height);
+	}
+	
+	public void setMap(int[][] map) {
+		map = genMap.getMap();
 	}
 	
 	
